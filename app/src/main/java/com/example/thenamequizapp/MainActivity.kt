d9be898b7b084f0b78b3e08f0264492ac2922ac3
JustEivas <181219@stud.hvl.no>
2020-02-26 11:05:46 +0100

    IT FREAKING WORKS!!!

--- app/src/main/java/com/example/thenamequizapp/MainActivity.kt
+++ app/src/main/java/com/example/thenamequizapp/MainActivity.kt
@@ -12,8 +12,11 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import com.google.android.gms.ads.MobileAds
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,6 +28,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lateinit var mAdView : AdView
+
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+
 
         // setup preference
         val pref = PreferenceManager.getDefaultSharedPreferences(this)
